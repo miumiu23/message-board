140311f1076f456d257f1f9e33eb9844f0909f14
fumitsujisawa <d.latina.23f@gmail.com>
2016-02-11 10:03:24 +0000

    submit

--- config/routes.rb
+++ config/routes.rb
@@ -49,11 +49,4 @@ Rails.application.routes.draw do
   #   end
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
