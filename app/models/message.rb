ff3603a2955bce7fa705e72da11b6db51d22844e
fumitsujisawa <d.latina.23f@gmail.com>
2016-02-16 09:12:59 +0000

    migration

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
-  # 内容は必須入力かつ2文字以上30文字以下
+# 年齢は必須入力かつ０歳以上120歳以下
+  validates :name ,numericality => true, :inclusion => { :in => 0..120 } , presence: true  
+ # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
